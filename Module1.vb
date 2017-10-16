f8b8d662fb57682cea0c0587be8c71bb769a847e
NatanFelipeM <32260480+NatanFelipeM@users.noreply.github.com>
2017-10-16 11:15:42 +0200

    prubataller1

---
+++ Module1.vb
@@ -0,0 +1,35 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim numx, numy As Double
+        Dim suma As Double
+        Dim resta As Double
+        Dim multiplicacion As Double
+
+        Console.WriteLine("Coloque los números que quiere calcular:")
+
+        numx = Convert.ToDouble(Console.ReadLine())
+        numy = Convert.ToDouble(Console.ReadLine())
+        suma = numx + numy
+        resta = numx - numy
+        multiplicacion = numx * numy
+
+
+        Console.WriteLine("Los resultados son: ")
+        Console.WriteLine("suma." & suma & " resta." & resta & " multiplicación." & multiplicacion)
+
+        Console.ReadLine()
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
